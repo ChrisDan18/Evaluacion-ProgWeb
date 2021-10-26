ff34582338bd74a6f9da66d868e0b79aacc873ae
ChrisDan18 <82722850+ChrisDan18@users.noreply.github.com>
2021-10-26 12:14:34 -0300

    Add files via upload

--- Evaluación de programación/eventos.js
+++ Evaluación de programación/eventos.js
@@ -1,12 +1,6 @@
 const idcontenido =document.getElementById('contenido');
 const classcajita =document.getElementsByClassName('cajita');
 
-let cajitas = [];
-cajitas.push('500');
-
-let elementoscajitas = document.getElementsByClassName('cajita');
-let arraycajitas = Array.from(cajitas);
-
 for (let i = 0; i < 500; i++) {
 
     const nuevacaja = document.createElement('div');
